02b86c6a0e369cc11788fc7f1ec05358dc2dff0d
Andres <andressocorro1@gmail.com>
2020-11-06 21:41:56 -0600

    Pulled data

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -4,12 +4,13 @@ var tableData = data;
 // YOUR CODE HERE!
 // console.log(tableData)
 
+// Add variable for tbody
+var tbody = d3.select("tbody");
+
 // Add Data to HTML Site
 tableData.forEach(ufoSighting => {
-    console.log(ufoSighting);
-
-
-    var tbody = d3.select("tbody");
+    // console.log(ufoSighting);
+    
 
     var row = tbody.append("tr");
 
@@ -22,3 +23,33 @@ tableData.forEach(ufoSighting => {
 // Connect button as an event/listener to filter the data
 var button = d3.select("#filter-btn");
 
+
+// When button is clicked do the following
+button.on("click", function() {
+
+    var dataCheck = 1
+
+    // Select data in datetime box
+    var inputFieldDate = d3.select("#datetime")
+
+    // grab the value property from the datetime box content
+    var inputDate = inputFieldDate.property("value");
+    // console.log(inputDate)
+
+    // filter and add the data
+    var filterDate = tableData.filter(ufoSighting => ufoSighting.datetime === inputDate);
+    console.log(filterDate);
+
+    if (dataCheck == 1) {
+        delTable = tbody.html("");
+    };
+
+    filterDate.forEach(ufoFiltered => {
+        var row = tbody.append('tr');
+        Object.values(ufoFiltered).forEach(value => {
+            row.append('td').text(value);
+        });
+
+    });
+    
+})
