7798536fe37c9975c8613959f8f41abbd3a6c405
Andres <andressocorro1@gmail.com>
2020-11-06 23:29:37 -0600

    updated UFO 2

--- UFO-Level-2/static/js/app.js
+++ UFO-Level-2/static/js/app.js
@@ -30,16 +30,31 @@ button.on("click", function() {
     var dataCheck = 1
 
     // Select data in datetime box
-    var inputFieldDate = d3.select("#datetime")
+    var inputFieldDate = d3.select("#datetime");
+    var inputFieldCity = d3.select("#city");
+    var inputFieldState = d3.select("#state");
+    var inputFieldCountry = d3.select("#country");
+    var inputFieldShape = d3.select("#shape");
 
     // grab the value property from the datetime box content
     var inputDate = inputFieldDate.property("value");
     // console.log(inputDate)
+    var inputCity = inputFieldCity.property("value").toLowerCase();
+    // console.log(inputCity);
+    var inputState = inputFieldState.property("value").toLowerCase();
+    var inputCountry = inputFieldCountry.property("value").toLowerCase();
+    var inputShape = inputFieldShape.property("value").toLowerCase();
 
-    // filter and add the data
+    // filter and add the data 
+
+    // ADD THE REST OF THE CRITERIA AS WELL AS BELOW (COMBINED) - THEN APPEND TO TD USE CITY AS EXAMPLE
     var filterDate = tableData.filter(ufoSighting => ufoSighting.datetime === inputDate);
-    console.log(filterDate);
+    // console.log(filterDate);
+    var filterCity = tableData.filter(ufoSighting => ufoSighting.city === inputCity);
+    // console.log(filterCity);
 
+    var filterCombined = tableData.filter(ufoSighting => ufoSighting.datetime === inputDate && ufoSighting.city === inputCity);
+    console.log(filterCombined)
     if (dataCheck == 1) {
         delTable = tbody.html("");
     };
